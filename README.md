0c630745b279a895739d599ff9f797f690ae8126
Alexey <youth-design@yandex.ru>
2022-10-01 19:04:44 +0300

    Add folder path to output log

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Remove folder if it size more then %first argument of scrpt% MB.
+# Remove folder(first argument) if it size more then %second argument of scrpt% MB.
+
 Used to clean up the dist folder when its size is large
+
+Example: `sh main.sh /home/user/somefolder/ 5092`
--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 size=$(du -s $1 | awk '{print$1}')
 size=$((size/1024))
 echo "Initialize size dist check at: $(date)" >> /home/$USER/distSizeChecker.log
-echo "Dist size is: ${size} MB" >> /home/$USER/distSizeChecker.log
+echo "Dist ($1) size is: ${size} MB" >> /home/$USER/distSizeChecker.log
 if (( $size > $2 )); then
   echo "Delete dist folder" >> /home/$USER/distSizeChecker.log
   rm -rf $1
